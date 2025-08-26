a9dff4c5b9e41ea6307664ea6b609dcb9e875755
philipppollak855 <philipp.pollak855@gmail.com>
2025-08-26 22:41:37 +0200

    test

--- backend/core/wsgi.py
+++ backend/core/wsgi.py
@@ -1,19 +1,9 @@
 # backend/core/wsgi.py
-# KORRIGIERT: Die Anwendung wird mit WhiteNoise umwickelt, um statische UND Mediendateien in der Produktion korrekt auszuliefern.
+# KORRIGIERT: Die manuelle WhiteNoise-Integration wurde entfernt.
+# Die Middleware und die Einstellungen in 'production.py' sind ausreichend und die empfohlene Methode.
 
 import os
 from django.core.wsgi import get_wsgi_application
-from django.conf import settings
-from whitenoise import WhiteNoise
 
-# Standard-Anwendung laden
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 application = get_wsgi_application()
-
-# WhiteNoise-Wrapper fÃƒÂ¼r statische und Mediendateien
-# Dieser Code wird nur ausgefÃƒÂ¼hrt, wenn DEBUG=False ist (also in der Produktion auf Render)
-if not settings.DEBUG:
-    # WhiteNoise anweisen, die statischen Dateien aus STATIC_ROOT zu bedienen
-    application = WhiteNoise(application, root=settings.STATIC_ROOT)
-    # WhiteNoise anweisen, ZUSÃƒâ€žTZLICH die Mediendateien aus MEDIA_ROOT zu bedienen
-    application.add_files(settings.MEDIA_ROOT, prefix=settings.MEDIA_URL)

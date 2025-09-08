96d8a0eb8ff0bfa1a01f0d014d7e35385b023d3a
philipppollak855 <philipp.pollak855@gmail.com>
2025-09-08 22:02:51 +0200

    feat: 'Mein Bereich' mit neuer Navigation und Platzhaltern ausbauen

--- frontend/src/modules/user/MeinBereich.jsx
+++ frontend/src/modules/user/MeinBereich.jsx
@@ -1,6 +1,6 @@
 // frontend/src/modules/user/MeinBereich.jsx
-// KORRIGIERT: Der Link "Meine Gedenkseite" wird jetzt immer angezeigt.
-// ERWEITERT: Neue Links und Routen für "Gespeicherte Seiten" und "Angehörige verwalten" hinzugefügt.
+// KORRIGIERT: Alle Navigationspunkte, einschließlich der Platzhalter, werden jetzt korrekt angezeigt.
+// ERWEITERT: "Meine Beiträge" wurde in die Seitennavigation integriert.
 
 import React, { useState, useEffect, useCallback } from 'react';
 import { NavLink, Routes, Route, Navigate } from 'react-router-dom';
@@ -15,8 +15,9 @@ import MeineDaten from './MeineDaten';
 import MeineMedien from './MeineMedien';
 import VerwalteteSeiten from './VerwalteteSeiten';
 import KontoVerwalten from './KontoVerwalten';
-// import GespeicherteSeiten from './GespeicherteSeiten'; // VORÜBERGEHEND DEAKTIVIERT
-// import AngehoerigeVerwalten from './AngehoerigeVerwalten'; // VORÜBERGEHEND DEAKTIVIERT
+import GespeicherteSeiten from './GespeicherteSeiten';
+import AngehoerigeVerwalten from './AngehoerigeVerwalten';
+import MyContributions from './MyContributions'; // NEU importiert
 
 const MeinBereich = () => {
     const [userData, setUserData] = useState({ own_page: null, managed_pages: [] });
@@ -51,10 +52,9 @@ const MeinBereich = () => {
                 <nav>
                     <NavLink to="vorsorge">Meine Vorsorge</NavLink>
                     <NavLink to="gedenkseite">Meine Gedenkseite</NavLink>
-                    {/* VORÜBERGEHEND DEAKTIVIERT 
                     <NavLink to="gespeicherte-seiten">Gespeicherte Seiten</NavLink>
                     <NavLink to="angehoerige">Angehörige verwalten</NavLink>
-                    */}
+                    <NavLink to="beitraege">Meine Beiträge</NavLink> {/* NEU */}
                     <NavLink to="daten">Meine Daten</NavLink>
                     <NavLink to="medien">Meine Medien</NavLink>
                     {userData.managed_pages && userData.managed_pages.length > 0 && (
@@ -66,12 +66,11 @@ const MeinBereich = () => {
             <main className="bereich-content">
                 <Routes>
                     <Route path="vorsorge" element={<MeineVorsorge />} />
-                    <Route path="gedenkseite" element={<MeineGedenkseite pageData={userData.own_page} />} />
+                    <Route path="gedenkseite" element={<MeineGedenkseite pageData={userData.own_page} onPageCreated={fetchUserData} />} />
                     <Route path="gedenkseite-erstellen" element={<MeineGedenkseiteErstellen onPageCreated={fetchUserData} />} />
-                    {/* VORÜBERGEHEND DEAKTIVIERT 
                     <Route path="gespeicherte-seiten" element={<GespeicherteSeiten />} />
                     <Route path="angehoerige" element={<AngehoerigeVerwalten />} />
-                    */}
+                    <Route path="beitraege" element={<MyContributions />} /> {/* NEU */}
                     <Route path="daten" element={<MeineDaten />} />
                     <Route path="medien" element={<MeineMedien />} />
                     <Route path="verwaltet" element={<VerwalteteSeiten pages={userData.managed_pages} />} />
